d67ab2286dbe591d35d6b6c6d9df1a6a51aff254
Mica Caputo <micaelacaputo98@gmail.com>
2024-11-22 13:02:08 -0300

    hotfix menu

--- Assets/Scripts/UI/CanvasCredits.cs
+++ Assets/Scripts/UI/CanvasCredits.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class CanvasCredits : MonoBehaviour
 {
     public Canvas creditsCanvas;
+    public Canvas buttonsCanvas;
 
     public void ShowCredits()
     {
         creditsCanvas.enabled = true;
+        buttonsCanvas.enabled = false;
     }
 
     public void HideCredits()
     {
         creditsCanvas.enabled = false;
+        buttonsCanvas.enabled = true;
     }
 }
